0b16d92aafc32fe490da1e4b63a296e1d6199b75
yoshiryo413 <yoshida.rio13@gmail.com>
2020-03-31 18:34:55 +0900

    commit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,6 +14,8 @@ class GroupsController < ApplicationController
       redirect_to root_path, notice: 'グループを作成しました'
     else
       render :new
+    end
+  end
 
   def edit
     @group = Group.find(params[:id])
